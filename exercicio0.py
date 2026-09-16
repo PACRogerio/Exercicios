cf1584ab5ecc778f3a138d4ab3878bb96078384e
unknown <rogpe@uol.com.br>
2026-09-16 17:51:50 -0300

    Descricao do que voce fez

--- exercicio0.py
+++ exercicio0.py
@@ -6,7 +6,8 @@ print("Para voltar a pasta anterior cd Exercicios")
 print
 print ("abra um folder no vscode")
 print ("Depois no vscode, criar um arquivo novo e coloca o nome do arquivo")
-print 
+print ("ou use New-Item nome.extensão")
+print("")
 print ("git add .")
 print ("git commit -m 'Solucao do Exercicio 1 com List Comprehension e NumPy'")
 print ("git push")
