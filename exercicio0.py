85bba6ec4ad532c846ce7e38a352dd7325a323a5
unknown <rogpe@uol.com.br>
2026-09-15 23:21:54 -0300

    Descricao do que voce fez

--- exercicio0.py
+++ exercicio0.py
@@ -1,4 +1,12 @@
 print ("Vamos começar!")
-#git add .
-#git commit -m "Solucao do Exercicio 1 com List Comprehension e NumPy"
-#git push
+print
+print ("primeiro cria um repositorio no git hub")
+print("cd Exercicios - entrar na pasta que voce quer")
+print("Para voltar a pasta anterior cd Exercicios")
+print
+print ("abra um folder no vscode")
+print ("Depois no vscode, criar um arquivo novo e coloca o nome do arquivo")
+print 
+print ("git add .")
+print ("git commit -m 'Solucao do Exercicio 1 com List Comprehension e NumPy'")
+print ("git push")
